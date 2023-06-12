17d2d0c1a806cd8053f777fa16d7c14394a4034b
araf <arafnoob@gmail.com>
2023-06-13 05:56:48 +0600

    Signin & Signup is done with the JWT token. Now need to intercept that token.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
   async singup(dto: AuthDto) {
     // generate the password hash
     const passHash = await argon.hash(dto.password);
@@ -22,9 +28,7 @@ export class AuthService {
       });
 
       // removing the password hash from the user object
-      delete user.hash;
-
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -35,18 +39,41 @@ export class AuthService {
     }
   }
 
-  signin(dto: AuthDto) {
-    try {
-      // find the user by email
-      // if not found throw error
+  async signin(dto: AuthDto) {
+    // find the user by email
+    // if not found throw error
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
 
-      // compare the password
-      // if not found throw error
+    if (!user) throw new ForbiddenException('Invalid Credentials');
 
-      // return user.
-      return 'I am signed in';
-    } catch (error) {
-      throw new HttpException('Something wrong with email and password.', 500);
-    }
+    // compare the password
+    // if not found throw error
+    const hasPassword = await argon.verify(user.hash, dto.password);
+
+    if (!hasPassword) throw new ForbiddenException('Invalid Credentials');
+
+    // return user.
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    return { access_token: token };
   }
 }
