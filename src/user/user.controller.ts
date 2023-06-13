809847bea493504380e8f4b2ba2aa295f5d0fcf6
araf <arafnoob@gmail.com>
2023-06-13 18:29:21 +0600

    user signin and singup is done with the jwt token.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
+  // @UseGuards by default use the 'jwt' AuthGuard.
   @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe() {
-    return 'hey I m in';
+  getMe(@Req() req: Request) {
+    return req.user;
   }
 }
