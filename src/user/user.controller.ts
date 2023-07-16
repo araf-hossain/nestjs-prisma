16496408d7ac584ecc21b6ca89db578367e8f7ed
araf <arafnoob@gmail.com>
2023-07-16 11:20:37 +0600

    customer param ddecorator added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   // @UseGuards by default use the 'jwt' AuthGuard.
-  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser('') user: User) {
+    return user;
   }
 }
